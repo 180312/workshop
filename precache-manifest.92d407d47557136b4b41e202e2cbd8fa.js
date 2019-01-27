97d73ca4f9f6078811a5593f35fc2dbc9212c94f
fortena <arnigeir@gmail.com>
2019-01-27 18:39:32 +0100

    Updates

---
+++ precache-manifest.92d407d47557136b4b41e202e2cbd8fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1f66262311b86653770",
+    "url": "/workshop/static/css/main.655f4a6e.chunk.css"
+  },
+  {
+    "revision": "b1f66262311b86653770",
+    "url": "/workshop/static/js/main.b1f66262.chunk.js"
+  },
+  {
+    "revision": "45082e8192724e675e0d",
+    "url": "/workshop/static/js/1.45082e81.chunk.js"
+  },
+  {
+    "revision": "a66ac1f97ff004b272d2",
+    "url": "/workshop/static/js/runtime~main.a66ac1f9.js"
+  },
+  {
+    "revision": "d7eb905bdadf763d99eb1fd8d6897cd6",
+    "url": "/workshop/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/workshop/precache-manifest.5c4e629a2c5aa774574815b377ab6bb8.js"
+  "/workshop/precache-manifest.92d407d47557136b4b41e202e2cbd8fa.js"
 );
 
 workbox.clientsClaim();
